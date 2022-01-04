bbd52391b3881e6f68d211e5e0d46a1ec103e4dd
hutyk7 <hutyk789@gmail.com>
2022-01-04 15:37:47 +0200

    js

--- js/modal.js
+++ js/modal.js
@@ -2,25 +2,28 @@
     document.addEventListener('DOMContentLoaded',function() {
     let requestButton = document.querySelector('.button_request');
   
-    const modalForm = document.querySelector('.modal');
-    
-    const closeButtonOk = modalForm.querySelector('.modal__close-button_ok');
+    let modalForm = document.querySelector('.modal');
   
-    const closeButton = modalForm.querySelector('.modal__close-button');
+    let closeButton = modalForm.querySelector('.modal__close-button');
   
     requestButton.addEventListener('click', function (e) {
       modalForm.classList.add('modal_active');
-    });
-  
-    closeButtonOk.addEventListener('click', function (e) {
-      e.preventDefault();
-      modalForm.classList.remove('modal_active');
+      tagBody.classList.add('hidden');
     });
   
     closeButton.addEventListener('click', function (e) {
       e.preventDefault();
       modalForm.classList.remove('modal_active');
+      tagBody.classList.add('hidden');
     });
+    requestButton.onmousedown = function (e) {
+      let target = e.target;
+      let modalContent = requestButton.getElementsByClassName('modal__content')[0];
+      if (e.target.closest('.' + modalContent.className) === null) {
+        this.classList.remove('modal_active');
+        tagBody.classList.remove('hidden');
+      }
+    };
   });
   })(); 
   
