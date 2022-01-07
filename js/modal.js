d54bf68cccac24cb903284f70f0aefbeeedff150
hutyk7 <hutyk789@gmail.com>
2022-01-07 17:45:15 +0000

    new wind

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
-(function () {
-    document.addEventListener('DOMContentLoaded',function() {
+
+    /*document.addEventListener('DOMContentLoaded',function() {
     const requestButton = document.querySelector('.button_request');
   
     const modalForm = document.querySelector('.modal');
@@ -13,9 +13,39 @@
     closeButton.addEventListener('click', function (e) {
       e.preventDefault();
       modalForm.classList.remove('modal_active');
-    });
+    });*/
+
+let testBut = document.querySelector(".testBut");
+  
+testBut.addEventListener('click', function() {
+  let idModal = this.getAttribute('data-modal-id');
+  openModal (idModal)
+})
+  
+function openModal(id_Modal) {
+  let modalWindow = document.querySelector(id_Modal);
+
+  if(modalWindow.classList.contains("active")) {
+    modalWindow.classList.remove('active');
+  }
+  else {
+    modalWindow.classList.add('active');
+  }
+
+  let butExit = modalWindow.querySelector('.butExit');
+
+  butExit.addEventListener('click', function() {
+    modalWindow.classList.remove('active');
+  })
+  let backBlack = modalWindow.querySelector('.backBlack');
+
+  backBlack.addEventListener('click', function() {
+    modalWindow.classList.remove('active');
+  })
+}
     
     
-  });
-  })(); 
+    
+
+
   
